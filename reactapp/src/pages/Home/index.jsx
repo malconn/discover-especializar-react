816cd26b5a41f5814209ca458ec22274fea1a543
Malcon Augusto Souza de Miranda <malcon.miranda@nm.local>
2022-04-07 13:13:13 -0300

    feat: propriedades e estilos (aulas feitas)

--- reactapp/src/pages/Home/index.jsx
+++ reactapp/src/pages/Home/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import './style.css';
+import { Card } from "../../components/Card";
 export function Home() {
   return (
     <div className="container">
       <h1>Lista de presença</h1>
       <input type="text" placeholder="Digite o nome ..."></input>
       <button type="button">Adicionar</button>
+      <Card name='Malcon' time='10:55:25'/>
+      <Card name='João' time='11:00:15'/>
     </div>
   );
 }
