4eb6cee4d3c4c23b1151add6c45df4f5f6aa829b
Malcon Augusto Souza de Miranda <malcon.miranda@nm.local>
2022-04-07 10:35:21 -0300

    feat: fragment e estilizando projeto (aulas feitas.)

--- reactapp/src/pages/Home.jsx
+++
@@ -1,5 +0,0 @@
-function Home() {
-  return <h1>Lista de presença</h1>;
-}
-
-export default Home;
---
+++ reactapp/src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Home() {
+  return (
+    <div>
+      <h1>Lista de presença</h1>
+      <li type="text" placeholder="Digite o nome ..."></li>
+      <button type="button">Adicionar</button>
+    </div>
+  );
+}
+
