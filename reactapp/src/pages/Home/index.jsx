3e43e90b1dc86f915d11ed4ba5c51cb7dd0d5327
Malcon Augusto Souza de Miranda <malcon.miranda@nm.local>
2022-04-07 11:12:29 -0300

    feat: estilizando pagina css (aula feita)

--- reactapp/src/pages/Home/index.jsx
+++ reactapp/src/pages/Home/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import './style.css';
 export function Home() {
   return (
-    <div>
+    <div className="container">
       <h1>Lista de presença</h1>
-      <li type="text" placeholder="Digite o nome ..."></li>
+      <input type="text" placeholder="Digite o nome ..."></input>
       <button type="button">Adicionar</button>
     </div>
   );
